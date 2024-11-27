7dd916821b924e78d33e585d6803bf8d39715b2e
Sampreet Patil <sampreetpatil270@gmail.com>
2024-11-27 22:06:08 +0530

    prac

---
+++ part.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+struct Particle {
+    float x, y;
+    float vx, vy;
+    float ax, ay;
+};
+
+const int WIDTH = 80;
+const int HEIGHT = 20;
+
+const float GRAVITY = 0.1f;
+
+const float DELTA_TIME = 0.1f;
+
+void clearScreen() {
+    std::cout << 
+}
